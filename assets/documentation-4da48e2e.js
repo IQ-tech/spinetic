15bc11b0bbf94b6bbd745f7d32b6788466ca1585
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-03 02:03:14 -0300

    Updates

--- assets/documentation-4da48e2e.js
+++ assets/documentation-4da48e2e.js
@@ -1,23 +1,23 @@
-import{j as n,a as t,F as a}from"./jsx-runtime-29545a09.js";import{M as l,P as c,C as h,b as s}from"./index-a9574396.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-6eb8df89.js";import"../sb-preview/runtime.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function r(i){const e=Object.assign({h1:"h1",h2:"h2",p:"p",ul:"ul",li:"li",a:"a",h3:"h3"},o(),i.components);return t(a,{children:[n(l,{isTemplate:!0}),`
-`,n(e.h1,{id:"spinetic--testtteeeeesss",children:"Spinetic  testtteeeeesss"}),`
+import{j as n,a as i,F as a}from"./jsx-runtime-29545a09.js";import{M as l,P as c,C as h,b as s}from"./index-e2fd5ae8.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-aa9b1c06.js";import"../sb-preview/runtime.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function r(t){const e=Object.assign({h1:"h1",h2:"h2",p:"p",ul:"ul",li:"li",a:"a",h3:"h3"},o(),t.components);return i(a,{children:[n(l,{isTemplate:!0}),`
+`,n(e.h1,{id:"spinetic",children:"Spinetic"}),`
 `,n("br",{}),`
 `,n(e.h2,{id:"overview",children:"Overview"}),`
 `,n(e.p,{children:"Spinetic allows you to easily integrate a carousel into your React application, providing a visually appealing way to showcase content. With minimal setup, you can create dynamic carousels that enhance the user experience."}),`
 `,n(e.h2,{id:"table-of-contents",children:"Table of Contents"}),`
-`,t(e.ul,{children:[`
+`,i(e.ul,{children:[`
 `,n(e.li,{children:n(e.a,{href:"?path=/docs/getting-started--documentation#spinetic---documentation",children:"Getting Started"})}),`
 `,n(e.li,{children:n(e.a,{href:"?path=/docs/getting-started--documentation#installation",children:"Installation"})}),`
 `,n(e.li,{children:n(e.a,{href:"?path=/docs/getting-started--documentation#example-usage",children:"Example Usage"})}),`
-`,t(e.li,{children:[n(e.a,{href:"#props",children:"Props"}),`
-`,t(e.ul,{children:[`
+`,i(e.li,{children:[n(e.a,{href:"#props",children:"Props"}),`
+`,i(e.ul,{children:[`
 `,n(e.li,{children:n(e.a,{href:"#propname1",children:"children"})}),`
 `,n(e.li,{children:n(e.a,{href:"#propname2",children:"config"})}),`
 `,n(e.li,{children:n(e.a,{href:"#propname2",children:"change"})}),`
 `]}),`
 `]}),`
 `,n(e.li,{children:n(e.a,{href:"#the-spinetic-playground",children:"The Spinetic Playground"})}),`
-`,t(e.li,{children:[n(e.a,{href:"#stories",children:"Stories - Examples"}),`
-`,t(e.ul,{children:[`
+`,i(e.li,{children:[n(e.a,{href:"#stories",children:"Stories - Examples"}),`
+`,i(e.ul,{children:[`
 `,n(e.li,{children:n(e.a,{href:"#default",children:"Default"})}),`
 `,n(e.li,{children:n(e.a,{href:"#auto-width",children:"Auto Width"})}),`
 `,n(e.li,{children:n(e.a,{href:"#auto-rotate",children:"Auto Rotate"})}),`
@@ -29,7 +29,7 @@ import{j as n,a as t,F as a}from"./jsx-runtime-29545a09.js";import{M as l,P as c
 `,n(e.h2,{id:"the-spinetic-playground",children:"The Spinetic Playground"}),`
 `,n(e.p,{children:`Welcome to the "Spinetic Playground"! Here, you can test the properties of your carousel and, at the end, export the settings you've tried out. It's all about customizing your carousel.`}),`
 `,n(c,{}),`
-`,t(e.p,{children:["These are all the properties that you can pass to ",n("strong",{children:"Spinetic"}),". Modify them and see how it looks. In the end, go to ",n("strong",{children:"Show Code"})," and copy the props."]}),`
+`,i(e.p,{children:["These are all the properties that you can pass to ",n("strong",{children:"Spinetic"}),". Modify them and see how it looks. In the end, go to ",n("strong",{children:"Show Code"})," and copy the props."]}),`
 `,n(h,{}),`
 `,n(e.h2,{id:"stories---examples",children:"Stories - Examples"}),`
 `,n(e.p,{children:"Here are some examples of configurations. Try expanding them or see if any examples suit your specific case."}),`
@@ -38,7 +38,7 @@ import{j as n,a as t,F as a}from"./jsx-runtime-29545a09.js";import{M as l,P as c
 `,n(e.h3,{id:"back-to-top",children:n(e.a,{href:"#spinetic",children:"Back to Top"})}),`
 `,n(e.p,{children:"quais as props o carrossel aceita, falar um pouco sobre..."}),`
 `,n(e.p,{children:"trocar PROPS por DEMO ou playground"}),`
-`,t(e.ul,{children:[`
+`,i(e.ul,{children:[`
 `,n(e.li,{children:n(e.a,{href:"#test",children:"verticalAlign"})}),`
 `]}),`
-`,n("button",{style:{color:"blue"},children:"ssss "})]})}function v(i={}){const{wrapper:e}=Object.assign({},o(),i.components);return e?n(e,Object.assign({},i,{children:n(r,i)})):r(i)}export{v as default};
+`,n("button",{style:{color:"blue"},children:"ssss "})]})}function v(t={}){const{wrapper:e}=Object.assign({},o(),t.components);return e?n(e,Object.assign({},t,{children:n(r,t)})):r(t)}export{v as default};
