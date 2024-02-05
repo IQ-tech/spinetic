1f7bda0caf2119d54996d37dda83cd80e5257b5e
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-05 10:13:04 -0300

    Updates

--- sb-addons/storybook-10/manager-bundle.js
+++ sb-addons/storybook-10/manager-bundle.js
@@ -1,3 +1,3 @@
 try{
-(()=>{(()=>{if(!sessionStorage.getItem("@storybook/manager/store")){let o=JSON.stringify({layout:{initialActive:"canvas",showToolbar:!1,isFullscreen:!0,showPanel:!0,showNav:!0,panelPosition:"bottom",showTabs:!0}});sessionStorage.setItem("@storybook/manager/store",o),alert("Welcome to the Spinentic documentation"),location.reload()}})();})();
+(()=>{(()=>{if(!sessionStorage.getItem("@storybook/manager/store")){let o=JSON.stringify({layout:{initialActive:"canvas",showToolbar:!1,isFullscreen:!0,showPanel:!0,showNav:!0,panelPosition:"bottom",showTabs:!0}});sessionStorage.setItem("@storybook/manager/store",o),location.reload()}})();})();
 }catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
