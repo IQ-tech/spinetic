3f90ee132f08482ac879bb83f36e680a84c7de62
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-04 21:59:03 -0300

    feat: :sparkles: add manager to storybook

---
+++ .storybook/manager.ts
@@ -0,0 +1,21 @@
+(() => {
+  const store = sessionStorage.getItem("@storybook/manager/store");
+  
+  if (!store) {
+    const defaultStore = JSON.stringify({ 
+      "layout": { 
+        "initialActive": "canvas", 
+        "showToolbar": false, 
+        "isFullscreen": true, 
+        "showPanel": true, 
+        "showNav": true, 
+        "panelPosition": "bottom", 
+        "showTabs": true 
+      } 
+    });
+
+    sessionStorage.setItem('@storybook/manager/store', defaultStore);
+    alert("Welcome to the Spinentic documentation");
+    location.reload();
+  }
+})();
