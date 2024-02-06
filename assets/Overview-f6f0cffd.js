eda5e6c1b26c90c5ade8a0c2d07ebd35b5588f0e
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-06 20:41:07 -0300

    Updates

--- assets/Overview-f6f0cffd.js
+++ assets/Overview-f6f0cffd.js
@@ -1,4 +1,4 @@
-import{j as e,a as i,F as r}from"./CardExample-d31eb566.js";import{M as c}from"./index-17c6f9e9.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-031a8f0d.js";import"../sb-preview/runtime.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function a(t){const n=Object.assign({h1:"h1",h2:"h2",p:"p",ul:"ul",li:"li",a:"a"},o(),t.components);return i(r,{children:[`
+import{j as e,a as i,F as r}from"./CardExample-d31eb566.js";import{M as c}from"./index-c25a4ddf.js";import{u as o}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-ec782373.js";import"../sb-preview/runtime.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";function a(t){const n=Object.assign({h1:"h1",h2:"h2",p:"p",ul:"ul",li:"li",a:"a"},o(),t.components);return i(r,{children:[`
 `,e(c,{title:"Pages/Overview"}),`
 `,e(n.h1,{id:"spinetic",children:"Spinetic"}),`
 `,e("br",{}),`
