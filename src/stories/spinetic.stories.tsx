3f0fbf281c4015dfb2ca2918e785af8818f29e87
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-07 01:29:08 -0300

    feat: :construction: function for tests

--- src/stories/spinetic.stories.tsx
+++ src/stories/spinetic.stories.tsx
@@ -39,33 +39,33 @@ const Template: StoryFn = (args: TypesConfigOptional | any) => {
 
   const config = { ...args };
 
+  // const [items, setItems] = useState<any>(childrens);
+  // console.log("🚀 ~ items:", items)
+  // const totalTimeInMilliseconds = 10 * 60 * 1000; // 10 minutos
+  // const intervalTime = 5 * 1000; // 10 segundos
 
-  const [items, setItems] = useState<any>(childrens);
-  const totalTimeInMilliseconds = 10 * 60 * 1000; // 10 minutos
-  const intervalTime = 5 * 1000; // 10 segundos
+  // useEffect(() => {
+  //   let elapsedTime = 0;
 
-  useEffect(() => {
-    let elapsedTime = 0;
+  //   const addItemInterval = setInterval(() => {
+  //     if (elapsedTime < totalTimeInMilliseconds) {
+  //       // Adiciona um novo item ao array
+  //      // setItems((prevItems: any) => [...prevItems, `Novo Item ${prevItems.length + 1}`]);// add
+  //       setItems((prevItems: any) => [...prevItems]);
+  //       elapsedTime += intervalTime;
+  //     } else {
+  //       // Se o tempo total for atingido, limpa o intervalo
+  //       clearInterval(addItemInterval);
+  //     }
+  //   }, intervalTime);
 
-    const addItemInterval = setInterval(() => {
-      if (elapsedTime < totalTimeInMilliseconds) {
-        // Adiciona um novo item ao array
-        //setItems((prevItems: any) => [...prevItems, `Novo Item ${prevItems.length + 1}`]);
-        setItems((prevItems: any) => [...prevItems]);
-        elapsedTime += intervalTime;
-      } else {
-        // Se o tempo total for atingido, limpa o intervalo
-        clearInterval(addItemInterval);
-      }
-    }, intervalTime);
-
-    // Limpa o intervalo quando o componente é desmontado
-    return () => clearInterval(addItemInterval);
-  }, [totalTimeInMilliseconds, intervalTime]);
+  //   // Limpa o intervalo quando o componente é desmontado
+  //   return () => clearInterval(addItemInterval);
+  // }, [totalTimeInMilliseconds, intervalTime]);
 
   return (
     <Spinetic config={config} change={change ? handleChange : undefined}>
-      {items.map((text: string, index: number) => (
+      {childrens.map((text: string, index: number) => (
         <SpineticItem key={index}>
           <CardExample highlightText={false} index={index} text={text} />
         </SpineticItem>
@@ -74,35 +74,43 @@ const Template: StoryFn = (args: TypesConfigOptional | any) => {
   );
 };
 
-//export const Default = Template.bind({});
+export const Default = Template.bind({});
 
-// export const AutoWidth = Template.bind({});
-// AutoWidth.args = {
-//   autoWidth: true,
-//   dotsModel: "long-rounded",
-//   fullHeightItems: true,
-// };
-
-// export const AutoRotate = Template.bind({});
-// AutoRotate.args = {
-//   autoRotate: true,
-//   dotsModel: "long-rounded",
-//   hideArrows: true,
-//   msPerAutoRotate: 2000,
-//   showItems: 3,
-// };
-
-export const Responsive = Template.bind({}); 
-Responsive.args = {
+export const AutoWidth = Template.bind({});
+AutoWidth.args = {
   autoWidth: true,
   dotsModel: "long-rounded",
+  fullHeightItems: true,
+};
+
+export const AutoRotate = Template.bind({});
+AutoRotate.args = {
+  autoRotate: true,
+  dotsModel: "long-rounded",
+  hideArrows: true,
+  msPerAutoRotate: 2000,
+  showItems: 3,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 450,
       settings: {
-        dots: false,
-        verticalAlign: true,
+        showItems: 1,
       },
     },
   ],
 };
+
+// export const Responsive = Template.bind({});
+// Responsive.args = {
+//   autoWidth: true,
+//   dotsModel: "long-rounded",
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         dots: false,
+//         verticalAlign: true,
+//       },
+//     },
+//   ],
+// };
