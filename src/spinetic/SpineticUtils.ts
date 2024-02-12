be2b1a524176bb90bf6ccbf307b03b05802212d4
Luiz Santos <70149879+Luiz-Sant0s@users.noreply.github.com>
2024-02-12 17:31:56 -0300

    feat: :sparkles: add childrenIsEqual in SpineticUtils

--- src/spinetic/SpineticUtils.ts
+++ src/spinetic/SpineticUtils.ts
@@ -1,71 +1,114 @@
 
-  /**
-   * @param target HTMLElement | null
-   * @description This method, called findElement, takes an HTMLElement or null as a parameter and returns an HTMLElement or null. It searches for a specific element with the class name "ls-carousel-item" by traversing up the DOM tree from the given target element. If the element is found, it is returned; otherwise, null is returned.
-   * @return HTMLElement | null
-   */
-  export const findElement = (target: HTMLElement | null): HTMLElement | null => {
-    let currentElement = target;
+/**
+ * @param target HTMLElement | null
+ * @description This method, called findElement, takes an HTMLElement or null as a parameter and returns an HTMLElement or null. It searches for a specific element with the class name "ls-carousel-item" by traversing up the DOM tree from the given target element. If the element is found, it is returned; otherwise, null is returned.
+ * @return HTMLElement | null
+ */
+export const findElement = (target: HTMLElement | null): HTMLElement | null => {
+  let currentElement = target;
 
-    while (currentElement && currentElement !== document?.documentElement) {
-      if (
-        currentElement.classList &&
-        currentElement.classList.contains("spinetic-item")
-      ) {
-        return currentElement;
-      }
-
-      currentElement = currentElement.parentElement!;
+  while (currentElement && currentElement !== document?.documentElement) {
+    if (
+      currentElement.classList &&
+      currentElement.classList.contains("spinetic-item")
+    ) {
+      return currentElement;
     }
 
-    return null;
+    currentElement = currentElement.parentElement!;
   }
 
-  /**
-   * @param array number[]
-   * @description The sumCarouselItemsWidths method takes an array of numbers as input and returns the sum of all the numbers in the array. The function uses the reduce method to iterate through the matrix and accumulate the values. The result is then rounded down using the Math.floor function before being returned.
-   * @return number
-   */
-  export const sumCarouselItemsWidths = (array: number[]): number => {
-    const sum = array?.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0
-    );
-    return Math.floor(sum);
-  }
+  return null;
+}
 
-  /**
-   *
-   * @param carouselItemsWidths number[]
-   * @param currentIndex number
-   * @description The calculateScrollAmount method takes an array of item widths and the current index as input. It returns the amount of scrolling needed to position the carousel at the current index.
-   * @returns number
-   */
-  export const  calculateScrollAmount = (
-    carouselItemsWidths: number[],
-    currentIndex: number
-  ): number => {
-    return Math.floor(carouselItemsWidths
-      ?.slice(0, currentIndex)
-      ?.reduce((total, width) => total - width, 0));
-  }
-
-  /**
- * Compares two arrays of numbers for equality.
- *
- * @param {number[]} array1 - The first array to compare.
- * @param {number[]} array2 - The second array to compare.
- * @returns {boolean} True if both arrays are of equal length and contain identical elements at each index, otherwise false.
- *
- * @example
- * const arrayA = [1, 2, 3];
- * const arrayB = [1, 2, 3];
- * const result = arraysAreEqual(arrayA, arrayB); // result is true
+/**
+ * @param array number[]
+ * @description The sumCarouselItemsWidths method takes an array of numbers as input and returns the sum of all the numbers in the array. The function uses the reduce method to iterate through the matrix and accumulate the values. The result is then rounded down using the Math.floor function before being returned.
+ * @return number
  */
-export const arraysAreEqual = (array1: any[] | any, array2: any[] | any) => {
+export const sumCarouselItemsWidths = (array: number[]): number => {
+  const sum = array?.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  return Math.floor(sum);
+}
+
+/**
+ *
+ * @param carouselItemsWidths number[]
+ * @param currentIndex number
+ * @description The calculateScrollAmount method takes an array of item widths and the current index as input. It returns the amount of scrolling needed to position the carousel at the current index.
+ * @returns number
+ */
+export const calculateScrollAmount = (
+  carouselItemsWidths: number[],
+  currentIndex: number
+): number => {
+  return Math.floor(carouselItemsWidths
+    ?.slice(0, currentIndex)
+    ?.reduce((total, width) => total - width, 0));
+}
+
+/**
+* Compares two arrays of numbers for equality.
+*
+* @param {number[]} array1 - The first array to compare.
+* @param {number[]} array2 - The second array to compare.
+* @returns {boolean} True if both arrays are of equal length and contain identical elements at each index, otherwise false.
+*
+* @example
+* const arrayA = [1, 2, 3];
+* const arrayB = [1, 2, 3];
+* const result = arraysAreEqual(arrayA, arrayB); // result is true
+*/
+export const arraysAreEqual = (array1: number[], array2: number[]) => {
+
   if (array1?.length !== array2?.length || !array1 || !array2) {
     return false;
   }
 
   return array1?.every((element: any, index: number) => element === array2[index]);
 };
+
+
+type Obj = { [key: string]: any } | undefined;
+function isObjEqual(obj1: Obj , obj2: Obj): boolean {
+  if (!obj1 || !obj2) return false;
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+
+    if (typeof value1 === 'object' && value1 !== null &&
+        typeof value2 === 'object' && value2 !== null) {
+      if (!isObjEqual(value1, value2)) {
+        return false;
+      }
+    } else {
+      if (value1 !== value2) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+type PropsChildren = { props?: { children?: { props?: Obj } } }[] | undefined | null | any;
+export function childrenIsEqual(children: PropsChildren, prevChildren: PropsChildren): boolean {
+if(!children || !prevChildren) return false;
+
+  return children?.every((child: PropsChildren, i: number) => {
+    const props1 = child?.props?.children?.props;
+    const props2 = prevChildren[i]?.props?.children?.props;
+    return isObjEqual(props1, props2);
+  }) || false;
+}
